d6c440d56d1bf6575c3e1400f24241eb15ca78a8
Glitch (rubeswiki) <none>
2020-11-07 21:50:38 +0000

    Imported project from Glitch

---
+++ server.js
@@ -0,0 +1,122 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var app = express();
+const hbs = require("hbs");
+app.use(cookieParser());
+app.set("view engine", "hbs");
+app.set("views", "views");
+hbs.registerPartials(__dirname + '/views/partials/');
+
+var multer  = require('multer');
+var upload = multer({ dest: __dirname + '/public/images/' });
+const querystring = require('querystring');
+const url = require('url');
+const fs = require('fs');
+const uuidv4 = require('uuid/v4');
+
+const projectUrl = 'https://' + process.env.PROJECT_DOMAIN + '.glitch.me';
+const apiManager = require('./apiManager');
+
+var apiResponse;
+
+/* Routes */
+
+app.use(express.static('public'));
+
+// Camera is default view
+app.get('/', (req, res) => {
+  res.render('camera', {
+    analyticsUrl: process.env.ANALYTICS_URL
+  });
+});
+
+// This route works for both the async request from the frontend
+// or as a form submittion if the fancy uploader doesn't work (no js).
+// At the end the image is deleted from the server
+app.post('/info', upload.single('file'), async function(req, res) {
+  var imagePath = false;
+  if (req.file && req.file.filename) {
+    imagePath = '/images/' + req.file.filename; 
+  } else {
+    apiResponse = {
+      error: true,
+      errorMessage: "No image file."
+    }
+  }
+  
+  if (imagePath) {
+    try {
+      apiResponse = await apiManager(imagePath, req, res);
+    } catch(e) {
+      apiResponse = {
+        error: true,
+        errorMessage: "API requests failed."
+      }
+    }
+  }
+  
+  if (!apiResponse.error) {
+    if (req.body.async) {
+      res.json({
+        error: false,
+        googleVisionGuess: apiResponse.gvBestGuess,
+      });
+    } else {
+      res.render('info', {
+        googleVisionGuess: apiResponse.gvBestGuess,
+        analyticsUrl: process.env.ANALYTICS_URL
+      });
+    }
+  } else {
+    if (req.body.async) {
+      res.json({
+        error: true,
+        errorMessage: apiResponse.errorMessage
+      });
+    } else {
+      handleError(res, "Error: " + apiResponse.errorMessage);
+    }
+  }
+  if (imagePath) {
+    try {
+      fs.unlinkSync('/app/public' + imagePath);
+    } catch (err) {
+      console.log('error deleting ' + imagePath + ': ' + err);
+    }
+  }
+
+});
+
+// Once the async apiManager request returns, frontend redirects to info
+app.get('/info', function(req,res) {
+  if (req.query.googleVisionGuess) {
+    res.render('info', {
+      background: apiResponse.wiki.image,
+      wikiTitle: apiResponse.wiki.title,
+      wikiUrl: apiResponse.wiki.url,
+      wikiInfo: apiResponse.wiki.info,
+      wikiSummary: apiResponse.wiki.summary,
+      wikiContent: apiResponse.wiki.content,
+      googleVisionGuess: req.query.googleVisionGuess,
+      analyticsUrl: process.env.ANALYTICS_URL
+    })
+  } else {
+    res.redirect('/');
+  }
+  
+});
+
+// General error handling
+function handleError(res, err) {
+  console.log("\nError");
+  console.log(JSON.stringify(err));
+  res.redirect('/error');
+}
+
+app.get('/error', function(req, res) {
+  
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('app is listening on port ' + listener.address().port);
+});
---
+++ wiki.js
@@ -0,0 +1,191 @@
+const fetch = require('node-fetch');
+const wiki = require('wikijs').default;
+
+async function handleSubmit(searchQuery) {
+  console.log("Wiki search query: ", searchQuery);  
+
+  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+  await fetch(endpoint)
+  		.then(response => response.json())
+  		.then(data => {
+  	  	const results = data.query.search;
+        const result = results[0]; 
+        this.resultTitle = result.title;
+		})
+    .catch(error => console.log('A Wiki Search error occurred: ', error));  
+  
+  console.log("title found: ", this.resultTitle);
+
+  this.wiki = {
+    title: "",
+    url: "",
+    image: "",
+    summary: "",
+    content: ""
+  };
+  
+  this.wiki.title = this.resultTitle;
+  
+  /*                             Url                                  */ 
+  this.wiki.url = await getWikiUrl(this.wiki.title);
+  console.log("Wiki URL: ", this.wiki.url);
+  
+  /*                             Image                                  */ 
+  this.wiki.image = await getWikiImage(this.wiki.title);
+  
+  //await sleep(1000); 
+  
+  /*                             Summary                                  */ 
+  this.wiki.summary = await getWikiSummary(this.wiki.title); 
+  
+  //await sleep(4000); 
+
+  /*                             Content                                  */ 
+  this.wiki.content = await getWikiContent(this.wiki.title, this.wiki.url);  
+  console.log("fin");
+  return this.wiki;
+}
+
+async function getWikiUrl(resultTitle) {
+  let wikiUrl = await wiki()
+                  .page(resultTitle)
+                  .then(page => page.url())
+                  .catch(() => console.log('Error fetching wiki URL'));
+  
+  if (wikiUrl) {
+    return wikiUrl;
+  } else {
+    return "unable to fetch wiki url";
+  }  
+}
+
+async function getWikiInfo(resultTitle) {
+  let wikiInfo = await wiki()
+                  .page(resultTitle)
+                  .then(page => page.info())
+                  .catch(error => console.log('Error fetching wiki INFO: ', error));
+  
+  if (wikiInfo) {
+    return wikiInfo;
+  } else {
+    return "unable to fetch wiki info";
+  }  
+}
+
+async function getWikiSummary(resultTitle) {
+  let wikiSummary = await wiki()
+                      .page(resultTitle)
+                      .then(page => page.summary())
+                      .catch(error => console.log('Error fetching wiki SUMMARY: ', error));
+  
+  if (wikiSummary) {
+    return shortenText(wikiSummary, 5);
+  } else { 
+    return "unable to fetch wiki summary (MediaWiki API uncooperative)";
+  } 
+}
+
+async function getWikiImage(resultTitle) {
+  let wikiImageList = await wiki()
+                      .page(resultTitle)
+                      .then(page => page.images())
+                      .catch(error => console.log('Error fetching wiki IMAGE LIST: ', error));
+  
+  if (wikiImageList) {
+    return getValidWikiImage(wikiImageList);
+  } else {
+    return "unable to fetch wiki image list";
+  }
+}
+
+function getValidWikiImage(wikiImageList) {
+  let extension = '';
+  
+  for (let i = 0; i < wikiImageList.length; i++) {
+    extension = wikiImageList[i].split(".").slice(-1)[0].toUpperCase(); 
+    if (extension === 'JPG' || extension === 'PNG') {
+      return wikiImageList[i];
+    }
+  }
+}
+
+async function getWikiContent(resultTitle, url) {
+  let rawWikiContent = await wiki()
+                      .page(resultTitle)
+                      .then(page => page.content())
+                      .catch(error => console.log('Error fetching wiki CONTENT: ', error));
+  
+  if (!rawWikiContent) {
+    return "unable to fetch wiki content";
+  }
+  
+  return parseValidWikiContent(buildValidWikiContent(rawWikiContent), url); 
+}
+
+function buildValidWikiContent(rawWikiContent) {
+  let i = 0;
+  let validWikiContent = [];  
+  while(i < rawWikiContent.length) {
+    if (
+        rawWikiContent[i].title && rawWikiContent[i].content &&
+        typeof rawWikiContent[i].content === 'string' &&
+        rawWikiContent[i].content.length >= 200
+       ) {
+          validWikiContent.push(rawWikiContent[i]);
+    }
+    i++;
+  }
+  
+  return validWikiContent; 
+}
+
+function parseValidWikiContent(validWikiContent, url) {
+  let j = 0;
+  let wikiContent = [];
+  
+  try {
+    while (j < validWikiContent.length && j < 3) {
+      wikiContent.push(
+         {
+           title: validWikiContent[j].title,
+           content: shortenText(validWikiContent[j].content, 4),
+           url: url + "#" + getWikiExtension(validWikiContent[j].title)
+         }
+       )
+      j++;
+    }
+  } catch (e) {
+    console.log("an error has occured establishing wiki content: ", e);
+  }
+  
+  return wikiContent;
+}
+
+function shortenText(originalText, maxLength) {
+  var summarySplit = originalText.match(/[^\.!\?]+[\.!\?]+/g);
+  let i = 0;
+  let shortenedText = "";
+  
+  if (summarySplit) {
+    while(i < maxLength && summarySplit[i]) {
+      shortenedText += summarySplit[i];
+      i++
+    }
+  } else {
+    shortenedText = "(no raw content to fetch)";
+  }
+  
+  return shortenedText;
+}
+
+function getWikiExtension(title) {
+  return title.split(' ').join('_');
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+module.exports = {
+  handleSubmit: handleSubmit
+}
